6f61b444c369e3c38a1bc57a7d73d0183427f53e
Juan Carlos Campos <rapidclimate@gmail.com>
2018-09-11 17:21:26 -0500

    Yo

---
+++ src/main/java/unitec/ivan/IvanApplication.java
@@ -0,0 +1,12 @@
+package unitec.ivan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IvanApplication.class, args);
+	}
+}
